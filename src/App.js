3982c79196e6c4df069787b2fe5d89f05f6246c8
EloraBarua <elorabarua62@gmail.com>
2022-07-19 22:26:53 +0600

    Header updated

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
             </RequireAuth>        
           }></Route>
                
-         <Route path='/blog' element={<Blog></Blog>}></Route>
+         {/* <Route path='/blog' element={<Blog></Blog>}></Route> */}
          <Route path='/about' element={<About></About>}></Route>
          <Route path='*' element={<NotFound></NotFound>}></Route>
         
